81f9f484e7d781941015b8df1bf8138b6cb47973
Steven Mayrand <smayrand@c1r4p5.42quebec.com>
2022-04-25 15:55:04 -0400

    Starting Commit

---
+++ libprintf.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libprintf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smayrand <smayrand@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/22 11:47:24 by smayrand          #+#    #+#             */
+/*   Updated: 2022/04/25 15:23:02 by smayrand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBPRINTF_A
+# define LIBPRINTF_A
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+typedef struct s_print
+{
+	
+}	t_print;
+
+int	ft_printf(const char *fmt, ...)
